34abf72b8e88f24b77bd37efe5e0886d6084d7e4
Stavros Kesov <112382419+KesovStavros@users.noreply.github.com>
2026-05-03 09:28:19 +0300

    b,,,,,,,m

--- engine/cookies.js
+++ engine/cookies.js
@@ -1,12 +1,22 @@
-function getcookie(name){
-    cookies=decodeURIComponent(document.cookie).split(";")
-    for(var i=0; i<cookies.lenght; i++){
-        var cookie=cookies[0]
-        while(cookie.charAt(0)==" "){
-            cookie=cookie.substring(1)
-        }
-        if(cookie.indexOf(name+"=")==0){
-            return cookie.substring(name.lenght+1, cookie.lenght)
-        }
+function getcookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-};
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//got from w3 site
